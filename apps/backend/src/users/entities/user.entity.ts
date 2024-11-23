24fac275b326e6bfa7805bd5b2ab1a66b8f8c7bd
Bálint Király <balintkiraly.dev@gmail.com>
2024-11-23 02:32:52 +0100

    User class validation

--- apps/backend/src/users/entities/user.entity.ts
+++ apps/backend/src/users/entities/user.entity.ts
@@ -1,1 +1,29 @@
-export class User {}
+import { Role } from '@prisma/client';
+import { IsBoolean, IsEmail, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString } from 'class-validator';
+
+export class User {
+  @IsNumber()
+  @IsPositive()
+  id: number;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsOptional()
+  phone: string;
+
+  @IsBoolean()
+  isDormResident: boolean;
+
+  @IsString()
+  @IsOptional()
+  roomNumber: string;
+
+  @IsEnum(Role)
+  role: Role;
+}
