62f3cbff70a52fb14af3de9bbb8c834402e43b8d
justnyx <bakonyiattilaksi04@gmail.com>
2024-12-07 17:40:41 +0100

    updated changed entities

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,10 +3,12 @@ import { PrismaModule } from 'nestjs-prisma';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommentsModule } from './comments/comments.module';
+import { ReservationsModule } from './reservations/reservations.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule.forRoot({ isGlobal: true }), UsersModule],
+  imports: [PrismaModule.forRoot({ isGlobal: true }), UsersModule, ReservationsModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
