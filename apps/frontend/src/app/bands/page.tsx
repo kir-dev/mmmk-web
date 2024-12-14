b9ec442dfb9b7051b9ea5d41b11b269574f4a604
Gergő Bujdosó <bujgergo@gmail.com>
2024-12-14 14:08:42 +0100

    added filtering

--- apps/frontend/src/app/bands/page.tsx
+++ apps/frontend/src/app/bands/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 import BandRow from '@/components/band/bandRow';
 import { Input } from '@/components/ui/input';
 import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table';
@@ -7,22 +9,26 @@ import { dummyBands } from '@/mocks/bands';
 
 export default function Bands() {
   const data = dummyBands;
-
+  const [filteredData, setFilteredData] = useState(data);
+  const [searchTerm, setSearchTerm] = useState('');
+  useEffect(() => {
+    setFilteredData(data.filter((band) => band.name.toLowerCase().includes(searchTerm.toLowerCase())));
+  }, [searchTerm]);
   return (
     <div className='w-full'>
-      <h1 className='text-2xl font-semibold p-4 text-orange-500'>Zenekarok</h1>
-      <div className='flex items-center py-4'>
+      <div className='flex items-center justify-between flex-row p-4'>
+        <h1 className='text-2xl font-semibold text-orange-500'>Zenekarok</h1>
         <Input
           placeholder='Keresés...'
-          value={(table.getColumn('email')?.getFilterValue() as string) ?? ''}
-          onChange={(event) => table.getColumn('email')?.setFilterValue(event.target.value)}
-          className='max-w-sm'
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+          className='max-w-sm text-black target:ring-0'
         />
       </div>
       <Table>
         <TableBody>
-          {data.length ? (
-            data.map((band) => <BandRow band={band} key={band.id} />)
+          {filteredData.length ? (
+            filteredData.map((band) => <BandRow band={band} key={band.id} />)
           ) : (
             <TableRow>
               <TableCell colSpan={4} className='h-24 text-center'>
