3b6b78a364679a1e63dc2b7eada5b8f55fa1c3ef
Bálint Király <balintkiraly.dev@gmail.com>
2024-12-23 06:07:04 +0100

    Add OpenAPI documentation
    Integrated Swagger for API documentation using OpenAPI in the backend.

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,15 +1,25 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Validation
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     })
   );
+
+  //OpenAPI
+  const config = new DocumentBuilder().setTitle('MMMK Web API').setVersion('1.0').addBearerAuth().build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(3001);
 }
+
 bootstrap();
