f1a65740266eb34b3f12e994541b570b2bc45fa4
justnyx <bakonyiattilaksi04@gmail.com>
2025-02-09 19:27:47 +0100

    refactored band backend

--- apps/backend/src/bands/bandsController.ts
+++ apps/backend/src/bands/bandsController.ts
@@ -1,55 +1,55 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 
-import { BandService } from './band.service';
+import { BandsService } from './bands.service';
 import { CreateBandDto } from './dto/create-band.dto';
 import { UpdateBandDto } from './dto/update-band.dto';
 
-@Controller('band')
-export class BandController {
-  constructor(private readonly bandService: BandService) {}
+@Controller('bands')
+export class BandsController {
+  constructor(private readonly bandsService: BandsService) {}
 
   @Post()
   create(@Body() createBandDto: CreateBandDto) {
-    return this.bandService.create(createBandDto);
+    return this.bandsService.create(createBandDto);
   }
 
   @Get()
   findAll() {
-    return this.bandService.findAll();
+    return this.bandsService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.bandService.findOne(id);
+    return this.bandsService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id', ParseIntPipe) id: number, @Body() updateBandDto: UpdateBandDto) {
-    return this.bandService.update(id, updateBandDto);
+    return this.bandsService.update(id, updateBandDto);
   }
 
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
-    return this.bandService.remove(id);
+    return this.bandsService.remove(id);
   }
 
   @Get(':id/members')
   findMembers(@Param('id', ParseIntPipe) id: number) {
-    return this.bandService.findMembers(id);
+    return this.bandsService.findMembers(id);
   }
 
   @Post(':id/members/:userId')
   addMember(@Param('id', ParseIntPipe) bandId: number, @Param('userId', ParseIntPipe) userId: number) {
-    return this.bandService.addMember(bandId, userId);
+    return this.bandsService.addMember(bandId, userId);
   }
 
   @Delete(':id/members/:userId')
   removeMember(@Param('id', ParseIntPipe) bandId: number, @Param('userId', ParseIntPipe) userId: number) {
-    return this.bandService.removeMember(bandId, userId);
+    return this.bandsService.removeMember(bandId, userId);
   }
 
   @Patch(':id/members/:userId')
   approveMember(@Param('id', ParseIntPipe) bandId: number, @Param('userId', ParseIntPipe) userId: number) {
-    return this.bandService.approveMember(bandId, userId);
+    return this.bandsService.approveMember(bandId, userId);
   }
 }
