572a0b3040036d87c0b4ac73d263630002744b2a
justnyx <bakonyiattilaksi04@gmail.com>
2025-02-09 19:34:15 +0100

    fixed import error

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,7 +3,7 @@ import { PrismaModule } from 'nestjs-prisma';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BandsModule } from './band/bandsModule';
+import { BandsModule } from './bands/bandsModule';
 import { CommentsModule } from './comments/comments.module';
 import { ReservationsModule } from './reservations/reservations.module';
 import { UsersModule } from './users/users.module';
