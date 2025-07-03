71545ff42aea6fd228e2257178051bd9ed7e3c3e
mozsarmate <mozsarmatee@gmail.com>
2025-07-03 20:51:51 +0200

    dto s

--- apps/backend/src/posts/entities/post.entity.ts
+++ apps/backend/src/posts/entities/post.entity.ts
@@ -1,1 +1,21 @@
-export class Post {}
+import { IsDate, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class Post {
+  @IsNotEmpty()
+  @IsNumber()
+  id: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  title: string;
+
+  @IsString()
+  body: string;
+
+  @IsNotEmpty()
+  @IsDate()
+  createdAt: Date;
+
+  @IsNumber()
+  authorId: number;
+}
